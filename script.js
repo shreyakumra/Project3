a603fe16e9af36d04a982f2011c39e8f1f8f1f99
shreyakumra <skumra@risd.edu>
2021-02-02 11:36:00 -0500

    new

--- script.js
+++ script.js
@@ -2,42 +2,37 @@ var block = document.getElementById("block");
 var character = document.getElementById("character");
 var hole = document.getElementById("hole");
 var coin = document.getElementById("coin");
-var counter=0;
-var jumping=0;
+var counter = 0;
+var jumping = 0;
 
 
 hole.addEventListener('animationiteration', () => {
-    let random = -((Math.random()*300)+150);
+    var random = -((Math.random()*300)+150);
     hole.style.top = random + "px";
     counter++;
 });
-var gravity = setInterval(function(){
-    var characterTop = 
-    parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+
+setInterval(function(){
+    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     if(jumping==0){
-        character.style.top = (characterTop+3) + "px";
+        character.style.top = (characterTop+3)+ "px";
     }
 
-    if (characterTop>480){
-        alert("Game over. Score: "+counter);
-        character.style.top = 100 + "px";
-        counter=0;
-    }
-
+  
 
     var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
     var holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
     var cTop = -(500-characterTop);
     if((characterTop>480)||((blockLeft<20)&&(blockLeft>-50)&&((cTop<holeTop)||(cTop>(holeTop+130))))) {
-//        alert("game over. score: "+counter);
-        character.style.top = 200 + "px";
+        alert("game over. score: "+(counter-1));
+        character.style.top = 100 + "px";
         counter = 0;
     }
 },10);
 
 function jump(){
     let jumpcounter = 0;
-    jumping=1;
+    jumping = 1;
     var jumpInterval = setInterval(function(){
         var characterTop = 
         parseInt(window.getComputedStyle(character).getPropertyValue("top"));
