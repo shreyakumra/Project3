aa0aaee89228ce1345ada8dc65fc468a0aaa4776
shreyakumra <skumra@risd.edu>
2021-02-02 12:45:13 -0500

    new

--- script.js
+++ script.js
@@ -15,23 +15,15 @@ hole.addEventListener('animationiteration', () => {
 setInterval(function(){
     var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     if(jumping==0){
-        character.style.top = (characterTop+3)+ "px";
+        character.style.top = (characterTop+3)+"px";
     }
-
-    if(characterTop>480){
-        alert("Game over. Score: "+counter);
-        character.style.top = 100 + "px";
-        counter = 0;
-    }
-
-
     var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
     var holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
     var cTop = -(500-characterTop);
-    if((characterTop>480)||((blockLeft<20)&&(blockLeft>-50)&&((cTop<holeTop)||(cTop>(holeTop+130))))) {
-        alert("game over. score: "(+counter));
+    if((characterTop>480)||((blockLeft<20)&&(blockLeft>-50)&&((cTop<holeTop)||(cTop>holeTop+130)))){
+        alert("Game over. Score: "+(counter-1));
         character.style.top = 100 + "px";
-        counter = 0;
+        counter=0;
     }
 },10);
 
